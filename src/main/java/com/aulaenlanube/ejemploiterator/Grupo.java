a70023505d699c73317ad1d99e31f68aecc3e33c
Edwin Porras A <edwin.porras@gmail.com>
2023-05-22 10:52:01 -0500

    Se mueve la clase Iterator al interior de la sobrecarga

--- src/main/java/com/aulaenlanube/ejemploiterator/Grupo.java
+++ src/main/java/com/aulaenlanube/ejemploiterator/Grupo.java
@@ -24,23 +24,19 @@ public class Grupo implements Iterable<Alumno> {
     @Override
     public Iterator<Alumno> iterator() {
         //return alumnos.iterator();
-        return new IteratorGrupo();
+        return new Iterator<Alumno>() {
+            private int posicion = 0;
 
-    }
+            @Override
+            public boolean hasNext() {
+                return posicion < alumnos.size();  //valida si hay otro alumno despues de la posicion actual
+            }
 
-    private class IteratorGrupo implements Iterator<Alumno> {
-
-        private int posicion = 0;
-
-        @Override
-        public boolean hasNext() {
-            return posicion < alumnos.size();  //valida si hay otro alumno despues de la posicion actual
-        }
-
-        @Override
-        public Alumno next() {
-            return alumnos.get(posicion++); //Devuelve el alumno de la siguiente posicion
-        }
+            @Override
+            public Alumno next() {
+                return alumnos.get(posicion++); //Devuelve el alumno de la siguiente posicion
+            }
+        };
     }
 
 }
